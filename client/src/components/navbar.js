a6492091a3affcf58add686892d0dd29a1d0a30f
Faruk Ohran <ohran.faruk@hotmail.com>
2020-05-04 16:52:01 +0200

    update

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -10,16 +10,16 @@ class nav extends Component {
     return (
       <div className="nav-border">
         <div className="container">
+          <div className="menuDivBtn">
+            <Button
+              onClick={() => this.setState({ clicked: !this.state.clicked })}
+              type="button"
+              className="btn btn-outline-secondary"
+            >
+              Menu
+            </Button>
+          </div>
           <div className="row w-100 justify-content-center ">
-            <div className="menuDivBtn">
-              <Button
-                onClick={() => this.setState({ clicked: !this.state.clicked })}
-                type="button"
-                className="btn btn-outline-secondary"
-              >
-                Menu
-              </Button>
-            </div>
             <div className="columna col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11">
               <div className={this.state.clicked ? "showNav" : "hideNav"}>
                 <ul className="nav nav-pills nav-fill navbar">
