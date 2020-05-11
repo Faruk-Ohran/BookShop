11deb1903a5372c204e6c774bfe9d3e3bb6c6499
Faruk Ohran <ohran.faruk@hotmail.com>
2020-05-11 13:26:08 +0200

    delete some components

--- client/src/components/Drama.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Drama() {
-  return <div></div>;
-}
-
-export default Drama;
--- client/src/components/Poetry.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Poetry() {
-  return <div></div>;
-}
-
-export default Poetry;
