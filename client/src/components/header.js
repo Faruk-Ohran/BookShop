c1b8a7c0db3f1d46a91eb79733736f94c18a68db
Faruk Ohran <ohran.faruk@hotmail.com>
2020-05-11 17:59:16 +0200

    some css updates

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Form, FormControl } from "react-bootstrap";
 
 class Header extends Component {
   render() {
