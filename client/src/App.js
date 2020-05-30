691813ed1205b031a5e731d459e7e26c574ad440
Faruk Ohran <ohran.faruk@hotmail.com>
2020-05-31 00:21:50 +0200

    added some css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Header from "./components/Header";
+import Header from "./components/header";
 import Home from "./components/Home";
 import Footer from "./components/footer";
 import Body from "./components/body";
